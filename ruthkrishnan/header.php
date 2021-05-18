6ed0d17f11c484d44d25000f0d940f5b8f40bb14
Matt Sprague <me@spragala.com>
2021-05-18 16:00:02 -0700

    updates(app): adds boilerplate code

---
+++ ruthkrishnan/header.php
@@ -0,0 +1,12 @@
+<?php
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width" />
+<title>Ruth Krishnan</title>
+
+<?php wp_head(); ?>
+</head>
+
+<body>
--- ruthkrishnan/index.php
+++ ruthkrishnan/index.php
@@ -1,1 +1,15 @@
+<?php
+
+get_header(); ?>
+
 <h1>Ruth Krishnan</h1>
+
+<?php
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+        echo get_the_content()
+    endwhile;
+endif;
+?>
+
+<?php get_footer(); ?>
