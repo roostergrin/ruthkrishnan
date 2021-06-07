d81f03e3e3ef8bc6db67c318ae1cfbeb50b07a82
Bryant Fukushima <bmfukushima94@gmail.com>
2021-06-07 16:32:53 -0700

    creates(page-neighborhoods): init page template for neighborhoods

---
+++ ruthkrishnan/page-neighborhoods.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Neighborhoods
+ * @package WordPress
+ * @subpackage ruthkrishnan
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div class="page-neighborhoods">
+  <?php get_template_part('template-parts/hero/site-hero'); ?>
+
+  
+</div>
+
+<?php get_footer(); ?>
