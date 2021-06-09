1f93932f880fdfd7fd5d998d02cd653ed9ea1cf4
Bryant Fukushima <bmfukushima94@gmail.com>
2021-06-09 12:12:22 -0700

    updates(test-map): add labels to test map

--- ruthkrishnan/icons/test-map.php
+++ ruthkrishnan/icons/test-map.php
@@ -10,9 +10,16 @@
 ?>
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 552 455" class='<?php echo $args["class"]; ?>'>
-  <g id="test-map" data-name="Layer 1">
-    <rect id="marina" class="map-neighborhoods__icon-neighborhood" y="4" width="320" height="271" fill="blue"/>
-    <rect id="soma" class="map-neighborhoods__icon-neighborhood" x="352" width="200" height="455" fill="#ed1c24"/>
-    <rect id="sunset-central-outer" class="map-neighborhoods__icon-neighborhood" y="307" width="320" height="148" fill="#009245"/>
+  <g id="marina" class="map-neighborhoods__icon-neighborhood">
+    <rect y="4" width="320" height="271" fill="blue"/>
+    <text transform="translate(221.98 252.98)" font-size="26" fill="#f2f2f2" font-family="MyriadPro-Regular, Myriad Pro"><tspan letter-spacing="0.01em">M</tspan><tspan x="21.06" y="0">a</tspan><tspan x="33.59" y="0" letter-spacing="0em">r</tspan><tspan x="42.2" y="0">ina</tspan></text>
+  </g>
+  <g id="sunset-central-outer" class="map-neighborhoods__icon-neighborhood">
+    <rect y="307" width="320" height="148" fill="#009245"/>
+    <text transform="translate(67.74 431.98)" font-size="26" fill="#f2f2f2" font-family="MyriadPro-Regular, Myriad Pro">Sunset <tspan x="79.01" y="0" letter-spacing="-0.01em">C</tspan><tspan x="93.81" y="0" letter-spacing="0em">e</tspan><tspan x="106.83" y="0" letter-spacing="0em">n</tspan><tspan x="121.16" y="0" letter-spacing="0em">t</tspan><tspan x="129.76" y="0" letter-spacing="0em">r</tspan><tspan x="138.14" y="0" letter-spacing="0em">al </tspan><tspan x="162.32" y="0" letter-spacing="0em">O</tspan><tspan x="180.31" y="0">u</tspan><tspan x="194.63" y="0" letter-spacing="-0.01em">t</tspan><tspan x="203.08" y="0" letter-spacing="0em">er</tspan></text>
+  </g>
+  <g id="soma" class="map-neighborhoods__icon-neighborhood">
+    <rect x="352" width="200" height="455" fill="#ed1c24"/>
+    <text transform="translate(457.98 431.98)" font-size="26" fill="#f2f2f2" font-family="MyriadPro-Regular, Myriad Pro">SO<tspan x="30.73" y="0" letter-spacing="-0.01em">M</tspan><tspan x="51.38" y="0" letter-spacing="0em">A</tspan></text>
   </g>
 </svg>
