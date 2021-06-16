3d24f508f3d824a5c1aa0cdc5da23102be4eff0d
Bryant Fukushima <bmfukushima94@gmail.com>
2021-06-16 11:38:26 -0700

    updates(single.php): add some content styles

--- ruthkrishnan/single.php
+++ ruthkrishnan/single.php
@@ -6,24 +6,22 @@
  * @version 1.0.0
  */
 
-    get_header(); ?>
+  get_header(); ?>
 
 <?php get_template_part('template-parts/hero/site-hero'); ?>
 
 <div class="post-blog">
-    <div class="post-blog__container">
-        <h1>Default Single</h1>
-
-        <?php
-            if ( have_posts() ) :
-                while ( have_posts() ) : the_post();
-                    echo the_title();
-                    echo get_field('content');
-                    // echo previous_post_link();
-                endwhile;
-            endif;
-        ?>
-    </div>
+  <div class="post-blog__container">
+    <?php
+      if ( have_posts() ) :
+        while ( have_posts() ) : the_post(); ?>
+          <div class="post-blog__content">
+            <?php echo get_field('content'); ?>
+          </div>
+        <?php endwhile;
+      endif;
+    ?>
+  </div>
 </div>
 
 <?php get_footer(); ?>
