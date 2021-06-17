59b73d9470ed35100379f31a9c84390e086a4345
Bryant Fukushima <bmfukushima94@gmail.com>
2021-06-17 09:15:06 -0700

    updates(test-map): new test map svg

--- ruthkrishnan/single.php
+++ ruthkrishnan/single.php
@@ -12,15 +12,34 @@
 
 <div class="post-blog">
   <div class="post-blog__container">
-    <?php
-      if ( have_posts() ) :
-        while ( have_posts() ) : the_post(); ?>
-          <div class="post-blog__content">
-            <?php echo get_field('content'); ?>
-          </div>
-        <?php endwhile;
-      endif;
-    ?>
+    <div class="post-blog__column">
+      <?php
+        if ( have_posts() ) :
+          while ( have_posts() ) : the_post(); ?>
+
+            <div class="post-blog__content">
+              <?php echo get_field('content'); ?>
+            </div>
+
+            <div class="post-blog__infobar">
+              <div class="post-blog__infobar-share">share</div>
+              <div class="post-blog__infobar-date"><?php echo get_the_date(); ?></div>
+              <div class="post-blog__infobar-category">
+                <a href="/blog/<?php echo get_the_category()[0]->slug; ?>">
+                  <?php echo get_the_category()[0]->name; ?>
+                </a>
+              </div>
+            </div>
+
+            <div class="post-blog__navigation">
+              <div class="post-blog__navigation-prev">previous</div>
+              <div class="post-blog__navigation-next">next</div>
+            </div>
+
+          <?php endwhile;
+        endif;
+      ?>
+    </div>
   </div>
 </div>
 
