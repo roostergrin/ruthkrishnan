9d50fd62157b81b4f0057462c7a8b6f2e1685833
Matt Sprague <me@spragala.com>
2021-06-22 16:44:09 -0700

    creates(testimonials): creates testimonials component

--- ruthkrishnan/front-page.php
+++ ruthkrishnan/front-page.php
@@ -10,6 +10,7 @@ get_header(); ?>
 <div class='site-wrapper'>
   <main id='content' role='main'>
     <h1>Home Page</h1>
+    <?php get_template_part('template-parts/testimonials/testimonials'); ?>
   </main>
 </div>
 
---
+++ ruthkrishnan/template-parts/testimonials/testimonials.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Client Testimonials.
+ *
+ * @package WordPress
+ * @subpackage ruthkrishnan
+ * @version 1.0
+ */
+
+?>
+
+<div class="testimonials-section">
+  <div class="testimonials-section__container">
+    <div class="testimonials-section__color-block"></div>
+
+    <div class="testimonials-section__col">
+      <div class="testimonials-section__images-slider" data-image-slider-length='<?php echo count(get_field('testimonials')); ?>'>
+        <?php if ( have_rows('testimonials') ) :
+           while ( have_rows('testimonials' ) ) : the_row();
+
+           $image = get_sub_field('image');
+
+           ?>
+          <div class="testimonials-section__image-slide" data-image-index='<?php echo get_row_index(); ?>'>
+            <?php echo wp_get_attachment_image($image, 'medium_large', false, [ 'class' => 'testimonials-section__image' ]); ?>
+          </div>
+          <?php endwhile; ?>
+        <?php endif; ?>
+      </div>
+    </div>
+
+    <div class="testimonials-section__content-col">
+      <div class="testimonials-section__content-slider">
+        <?php echo wp_get_attachment_image(get_field('background'), 'medium_large', false, [ 'class' => 'testimonials-section__background-image' ]); ?>
+        <div class="testimonials-section__title">
+          <h2><?php echo get_field('title')?></h2>
+        </div>
+        <div class="testimonials-section__content-wrapper">
+            <?php if ( have_rows('testimonials') ) :
+               while ( have_rows('testimonials' ) ) : the_row();
+
+               $text = get_sub_field('content');
+               $name = get_sub_field('name');
+
+               ?>
+              <div class="testimonials-section__content-slide" data-content-index='<?php echo get_row_index(); ?>'>
+                <div class="testimonials-section__content">
+                  <div class="testimonials-section__text"><?php echo $text ?></div>
+                  <div class="testimonials-section__name"><?php echo $name ?></div>
+                </div>
+              </div>
+              <?php endwhile; ?>
+            <?php endif; ?>
+          <div class="testimonials-section__indicators">
+            <?php
+              foreach (get_field('testimonials') as $key=>$dot) : ?>
+                <div class="testimonials-section__dot" data-index='<?php echo $key; ?>'></div>
+            <?php endforeach; ?>
+          </div>
+        </div>
+      </div>
+    </div> <!-- content-col -->
+  </div> <!-- container -->
+</div>
