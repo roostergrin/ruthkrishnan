15db8a0d2887f2200db98669f5961c7f7193c390
Matt Sprague <me@spragala.com>
2021-06-28 15:58:01 -0700

    updates(home): adds welcome section to the home page

--- ruthkrishnan/front-page.php
+++ ruthkrishnan/front-page.php
@@ -12,6 +12,46 @@ get_header(); ?>
     <div class="page-home">
       <div class="page-home__loader"></div>
       <?php get_template_part('template-parts/hero/home-hero'); ?>
+
+      <!-- Home Welcome Section -->
+
+      <section class="home-welcome">
+        <div class="home-welcome__color-box"></div>
+        <h2 class="home-welcome__title home-welcome__title--mobile">
+          <?php echo get_field('welcome_title') ?>
+        </h2>
+        <div class="home-welcome__container">
+          <div class="home-welcome__content">
+            <h2 class="home-welcome__title">
+              <?php echo get_field('welcome_title') ?>
+            </h2>
+            <div class="home-welcome__text">
+              <?php echo get_field('welcome_text') ?>
+            </div>
+            <a href='/about-us#meet-our-team' class="home-welcome__button">meet our team</a>
+          </div>
+          <div class="home-welcome__image-col">
+            <div class="home-welcome__image-container">
+              <?php echo wp_get_attachment_image(get_field('welcome_thumbnail'), 'medium_large', false, [ 'class' => 'home-welcome__thumbnail' ]); ?>
+              <div class="home-welcome__play-btn">
+                <?php get_template_part('icons/play', null, array('class' => 'home-welcome__icon')); ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div class="home-welcome__video-modal">
+        <div class="home-welcome__modal-overlay"></div>
+        <div class="home-welcome__modal-container">
+          <div class="home-welcome__close-btn">
+            <span></span>
+            <span></span>
+          </div>
+          <video class="home-welcome__video" src="<?php echo get_field('welcome_video') ?>" type='video/mp4' controls></video>
+        </div>
+      </div> <!-- End Home Welcome Section -->
+
       <?php get_template_part('template-parts/testimonials/testimonials'); ?>
     </div>
     <h1>Home Page</h1>
---
+++ ruthkrishnan/icons/play.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Play button.
+ *
+ * @package WordPress
+ * @subpackage ruthkrishnan
+ * @version 1.0
+ */
+
+ ?>
+
+ <svg data-name="play-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" class='<?php echo $args["class"]; ?>'>
+   <defs>
+     <style>
+       .bb9f18d9-bf5b-4452-8907-32651f9a8ba7 {
+         fill-rule: evenodd;
+       }
+     </style>
+   </defs>
+   <path class="bb9f18d9-bf5b-4452-8907-32651f9a8ba7" d="M32,64.11a32,32,0,1,0-32-32A32,32,0,0,0,32,64.11ZM26,22.41A1.33,1.33,0,0,0,24,23.6v17A1.34,1.34,0,0,0,26,41.82l17-8.51a1.34,1.34,0,0,0,0-2.39Z" transform="translate(-0.02 -0.11)"/>
+ </svg>
