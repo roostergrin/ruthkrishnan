72468527d9dcddbea700a8157ae510b77ea5ca8f
Bryant Fukushima <bmfukushima94@gmail.com>
2021-07-20 10:50:11 -0700

    updates(global): remove form subscribe func

--- ruthkrishnan/src/scripts/global.js
+++ ruthkrishnan/src/scripts/global.js
@@ -1,11 +1,11 @@
 import { navigation } from './resources/navigation'
 import { testimonials } from './resources/testimonials'
-import { formSubscribe } from './resources/form-subscribe'
+// import { formSubscribe } from './resources/form-subscribe'
 import { formGetInTouch } from './resources/form-get-in-touch'
 
 document.addEventListener('DOMContentLoaded', function () {
   navigation();
   testimonials();
-  formSubscribe();
+  // formSubscribe();
   formGetInTouch();
 });
