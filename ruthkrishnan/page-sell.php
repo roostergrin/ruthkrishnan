a7f981378fd1ec9d07411be73426bdf110eff8e0
Bryant Fukushima <bmfukushima94@gmail.com>
2021-07-20 20:16:23 -0700

    creates(our-performance): our performance template part

--- ruthkrishnan/page-sell.php
+++ ruthkrishnan/page-sell.php
@@ -25,6 +25,23 @@ get_header(); ?>
   </div>
   <!-- END Sell Welcome Section -->
 
+  <!-- Our Performance Template Part -->
+  <?php get_template_part('template-parts/our-performance/our-performance'); ?>
+  <!-- END Our Performance Template Part -->
+
+  <!-- Sell Prices Section -->
+  <div class="sell-prices">
+    <div class="sell-prices__container">
+      <div class="sell-prices__column">
+        <div class="sell-prices__content">
+          <h3 class="sell-prices__title">Sales Price to List Price</h3>
+        </div>
+        <!-- <?php echo wp_get_attachment_image(get_field('performance_image'), 'full', false, [ 'class' => 'sell-prices__image' ]); ?> -->
+      </div>
+    </div>
+  </div>
+  <!-- END Sell Prices Section -->
+
   <!-- Testimonials Template Part -->
   <?php get_template_part('template-parts/testimonials/testimonials'); ?>
   <!-- END Testimonials Template Part -->
