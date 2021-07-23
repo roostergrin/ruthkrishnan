8cf502e59b08c74e22378150ad73d255c823b3f1
Bryant Fukushima <bmfukushima94@gmail.com>
2021-07-23 14:56:49 -0700

    updates(page-talks): add intro section

--- ruthkrishnan/page-talks.php
+++ ruthkrishnan/page-talks.php
@@ -14,6 +14,24 @@ get_header(); ?>
   <?php get_template_part('template-parts/hero/site-hero'); ?>
   <!-- END Site Hero Template Part -->
 
+  <!-- Talks Intro Section -->
+  <div class="talks-intro">
+    <div class="talks-intro__container">
+      <div class="talks-intro__column">
+        <h2 class="talks-intro__title"><?php echo get_field('talks_intro_title'); ?></h2>
+        <div class="talks-intro__video-container">
+          <iframe class="talks-intro__video" data-src="<?php echo get_field('talks_intro_video') ?>?title=0&byline=0&portrait=0&autoplay=1" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+          <?php echo wp_get_attachment_image(get_field('talks_intro_thumbnail'), 'full', false, [ 'class' => 'talks-intro__thumbnail' ]); ?>
+          <div class="talks-intro__play-btn">
+            <?php get_template_part('icons/play', null, array('class' => 'talks-intro__icon')); ?>
+          </div>
+        </div>
+        <div class="talks-intro__text"><?php echo get_field('talks_intro_text'); ?></div>
+      </div>
+    </div>
+  </div>
+  <!-- END Talks Intro Section -->
+
   <!-- Talks What You'll Learn Section -->
   <div class="talks-learn">
     <div class="talks-learn__container">
--- ruthkrishnan/src/scripts/pages/talks.js
+++ ruthkrishnan/src/scripts/pages/talks.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-  
-  console.log('talks')
+  const playButton = document.querySelector('.talks-intro__play-btn'),
+        thumbnail = document.querySelector('.talks-intro__thumbnail'),
+        video = document.querySelector('.talks-intro__video');
+
+  playButton.addEventListener('click', () => {
+    video.src = video.dataset.src;
+    playButton.classList.add('talks-intro__play-btn--hidden');
+    thumbnail.classList.add('talks-intro__thumbnail--hidden');
+    video.classList.add('talks-intro__video--active');
+  })
 
 });
