8278b959dd60065c83d73c77b1afb7d7c58e4294
Bryant Fukushima <bmfukushima94@gmail.com>
2021-07-23 01:16:19 -0700

    updates(slider-neighborhoods): color animations on click

--- ruthkrishnan/template-parts/maps/neighborhoods-map.php
+++ ruthkrishnan/template-parts/maps/neighborhoods-map.php
@@ -11,12 +11,11 @@
 
 <div class="map-neighborhoods">
   <div class="map-neighborhoods__tooltip">
-    <div id="tooltip-close" class="map-neighborhoods__tooltip-close">X</div>
+    <div id="tooltip-close" class="map-neighborhoods__tooltip-close"></div>
     <div id="tooltip-content" class="map-neighborhoods__tooltip-content"></div>
   </div>
   <div class="map-neighborhoods__container">
     <div class="map-neighborhoods__column">
-      <h2>This is the Neighborhood Map</h2>
       <?php get_template_part('icons/map', null, array( 'class' => 'map-neighborhoods__icon' )); ?>
     </div>
   </div>
