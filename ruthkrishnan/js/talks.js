f3bb714050a174e43dca03b72b4dc3b37fdae8d4
Bryant Fukushima <bmfukushima94@gmail.com>
2021-07-26 14:47:10 -0700

    updates(page-talks): add more talks section

--- ruthkrishnan/js/talks.js
+++ ruthkrishnan/js/talks.js
@@ -3,6 +3,18 @@ var __webpack_exports__ = {};
 /*!************************************!*\
   !*** ./src/scripts/pages/talks.js ***!
   \************************************/
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 document.addEventListener('DOMContentLoaded', function () {
   var playButton = document.querySelector('.talks-intro__play-btn'),
       thumbnail = document.querySelector('.talks-intro__thumbnail'),
@@ -12,7 +24,242 @@ document.addEventListener('DOMContentLoaded', function () {
     playButton.classList.add('talks-intro__play-btn--hidden');
     thumbnail.classList.add('talks-intro__thumbnail--hidden');
     video.classList.add('talks-intro__video--active');
+  }); // Talks Video Slider -----------------------------------------------
+
+  var slidesData = Array.from(document.querySelectorAll('.talks-video-slider__slide')),
+      slidesContainer = document.querySelector('.talks-video-slider__slides'),
+      prevArrow = document.querySelector('.talks-video-slider__prev'),
+      nextArrow = document.querySelector('.talks-video-slider__next'),
+      indicatorsContainer = document.querySelector('.talks-video-slider__indicators'),
+      indicators = Array.from(document.querySelectorAll('.talks-video-slider__dot')),
+      playBtns = Array.from(document.querySelectorAll('.talks-video-slider__slide-play-btn'));
+  var slidesArr = [],
+      prevSlides = [],
+      currSlides = [],
+      nextSlides = [],
+      indicatorActive = 0,
+      debounceLastTimeout = null; // clear Slides Data
+
+  slidesContainer.innerHTML = '';
+
+  var createSlides = function createSlides() {
+    // create slides Array
+    slidesArr = slidesData.map(function (slide, i) {
+      return {
+        index: i,
+        el: slide,
+        video: slide.dataset.video,
+        thumbnail: slide.dataset.thumbnail,
+        title: slide.dataset.title
+      };
+    }); // clone slides
+
+    if (slidesArr.length === 4 && window.innerWidth > 768) {
+      slidesArr.forEach(function (slide, i) {
+        slidesArr.push({
+          index: i + 4,
+          el: slide.el.cloneNode(true),
+          video: slide.video,
+          thumbnail: slide.thumbnail,
+          title: slide.title
+        });
+      });
+    } else if (slidesArr.length === 2 && window.innerWidth < 769) {
+      slidesArr.forEach(function (slide, i) {
+        slidesArr.push({
+          index: i + 2,
+          el: slide.el.cloneNode(true),
+          video: slide.video,
+          thumbnail: slide.thumbnail,
+          title: slide.title
+        });
+      });
+    }
+  };
+
+  createSlides(); // remove indicators
+
+  var showIndicators = function showIndicators() {
+    if (slidesArr.length < 4 && window.innerWidth > 768 || slidesArr.length < 2 && window.innerWidth < 769) {
+      indicatorsContainer.classList.add('talks-video-slider__indicators--hidden');
+      prevArrow.classList.add('talks-video-slider__prev--hidden');
+      nextArrow.classList.add('talks-video-slider__next--hidden');
+    } else {
+      indicatorsContainer.classList.remove('talks-video-slider__indicators--hidden');
+      prevArrow.classList.remove('talks-video-slider__prev--hidden');
+      nextArrow.classList.remove('talks-video-slider__next--hidden');
+    }
+  };
+
+  showIndicators();
+
+  var appendSlides = function appendSlides() {
+    slidesArr.forEach(function (slide) {
+      slide.el.classList.remove('talks-video-slider__slide--prev');
+      slide.el.classList.remove('talks-video-slider__slide--curr');
+      slide.el.classList.remove('talks-video-slider__slide--next');
+      slide.el.dataset.index = slide.index;
+      slidesContainer.append(slide.el);
+    });
+    slidesArr = Array.from(document.querySelectorAll('.talks-video-slider__slide'));
+  };
+
+  appendSlides(); // create slide position array
+
+  var createSlideArrays = function createSlideArrays() {
+    if (window.innerWidth > 768) {
+      slidesArr.forEach(function (slide, i) {
+        if (i < 3) {
+          currSlides.push(i);
+        } else if (i === slidesArr.length - 1) {
+          prevSlides.push(i);
+        } else {
+          nextSlides.push(i);
+        }
+      });
+    } else {
+      slidesArr.forEach(function (slide, i) {
+        if (i < 1) {
+          currSlides.push(i);
+        } else if (i === slidesArr.length - 1) {
+          prevSlides.push(i);
+        } else {
+          nextSlides.push(i);
+        }
+      });
+    }
+  };
+
+  createSlideArrays(); // set slide classes and stylkes
+
+  var setSlides = function setSlides() {
+    slidesArr.forEach(function (slide) {
+      if (prevSlides.includes(+slide.dataset.index)) {
+        slide.classList.remove('talks-video-slider__slide--curr');
+        slide.classList.remove('talks-video-slider__slide--next');
+        slide.classList.add('talks-video-slider__slide--prev');
+        slide.style.opacity = 0;
+        slide.style.transform = 'translateX(-100%)';
+      } else if (currSlides.includes(+slide.dataset.index)) {
+        slide.classList.remove('talks-video-slider__slide--prev');
+        slide.classList.remove('talks-video-slider__slide--next');
+        slide.classList.add('talks-video-slider__slide--curr');
+        slide.style.opacity = 1;
+        slide.style.transform = "translateX(".concat(100 * currSlides.indexOf(+slide.dataset.index), "%)");
+      } else {
+        slide.classList.remove('talks-video-slider__slide--prev');
+        slide.classList.remove('talks-video-slider__slide--curr');
+        slide.classList.add('talks-video-slider__slide--next');
+        slide.style.opacity = 0;
+        slide.style.transform = 'translateX(300%)';
+      }
+    });
+  };
+
+  setSlides(); // set slide/slide container Height
+
+  var setHeight = function setHeight() {
+    var slides = Array.from(document.querySelectorAll('.talks-video-slider__slide')),
+        slideHeights = slides.map(function (slide) {
+      return slide.scrollHeight;
+    }),
+        maxHeight = Math.max.apply(Math, _toConsumableArray(slideHeights));
+    slidesContainer.style.height = maxHeight + 'px';
+  };
+
+  setHeight(); // set indicator active
+
+  var setIndicator = function setIndicator() {
+    indicators.forEach(function (dot) {
+      return dot.classList.remove('talks-video-slider__dot--active');
+    });
+    indicators[indicatorActive].classList.add('talks-video-slider__dot--active');
+  };
+
+  setIndicator(); // next slide functionality
+
+  var toNextSlide = function toNextSlide() {
+    nextSlides.push(prevSlides.pop());
+    prevSlides.unshift(currSlides.shift());
+    currSlides.push(nextSlides.shift());
+    setSlides();
+    indicatorActive < indicators.length - 1 ? indicatorActive++ : indicatorActive = 0;
+    setIndicator();
+  };
+
+  nextArrow.addEventListener('click', toNextSlide); // next slide functionality
+
+  var toPrevSlide = function toPrevSlide() {
+    nextSlides.unshift(currSlides.pop());
+    currSlides.unshift(prevSlides.shift());
+    prevSlides.push(nextSlides.pop());
+    setSlides();
+    indicatorActive > 0 ? indicatorActive-- : indicatorActive = indicators.length - 1;
+    setIndicator();
+  };
+
+  prevArrow.addEventListener('click', toPrevSlide); // debounce function
+
+  var debounce = function debounce(func, args, wait, immediate) {
+    var later = function later() {
+      debounceLastTimeout = null;
+
+      if (!immediate) {
+        func(args);
+      }
+    };
+
+    var callNow = immediate && !debounceLastTimeout;
+    clearTimeout(debounceLastTimeout);
+    debounceLastTimeout = setTimeout(later, wait);
+
+    if (callNow) {
+      func(args);
+    }
+  };
+
+  var reset = function reset() {
+    slidesContainer.innerHTML = '';
+    slidesArr = [];
+    prevSlides = [];
+    currSlides = [];
+    nextSlides = [];
+    indicatorActive = 0;
+    createSlides();
+    appendSlides();
+    slidesArr.forEach(function (slide) {
+      return slide.style = null;
+    });
+    createSlideArrays();
+    setSlides();
+    setHeight();
+    showIndicators();
+  };
+
+  window.addEventListener('resize', function () {
+    debounce(reset, null, 500);
   });
+  var slideContainers = Array.from(document.querySelectorAll('.talks-video-slider__slide-container')),
+      videoModalContainer = document.querySelector('.talks-video-slider__video-modal'),
+      videoModal = document.querySelector('.talks-video-slider__video'),
+      modalOverlay = document.querySelector('.talks-video-slider__modal-overlay'),
+      modalCloseBtn = document.querySelector('.talks-video-slider__close-btn');
+
+  var closeModal = function closeModal() {
+    videoModalContainer.classList.remove('talks-video-slider__video-modal--open');
+    videoModal.src = '';
+  };
+
+  slideContainers.forEach(function (slide, i) {
+    slide.addEventListener('click', function () {
+      videoModalContainer.classList.add('talks-video-slider__video-modal--open');
+      setTimeout(function () {
+        videoModal.src = slidesArr[i].dataset.video + '?title=0&byline=0&portrait&autoplay=1';
+      }, 250);
+    });
+  });
+  modalCloseBtn.addEventListener('click', closeModal);
+  modalOverlay.addEventListener('click', closeModal);
 });
 /******/ })()
 ;
--- ruthkrishnan/page-talks.php
+++ ruthkrishnan/page-talks.php
@@ -32,6 +32,63 @@ get_header(); ?>
   </div>
   <!-- END Talks Intro Section -->
 
+  <!-- Talks Video Slider -->
+  <?php if ( !empty(get_field('talks_video_slider_videos')) ) : ?>
+    <div class="talks-video-slider">
+      <div class="talks-video-slider__container">
+        <div class="talks-video-slider__column">
+          <h2 class="talks-video-slider__title"><?php echo get_field('talks_video_slider_title'); ?></h2>
+          <div class="talks-video-slider__slides">
+
+            <?php foreach (get_field('talks_video_slider_videos') as $key=>$slide) :
+              $video = $slide['video_src'];
+              $thumbnail = wp_get_attachment_image($slide['video_thumbnail'], 'medium_large', false, [ 'class' => 'talks-video-slider__slide-image' ]);
+              $title = $slide['video_title']; ?>
+
+              <div class="talks-video-slider__slide" data-video='<?php echo $video; ?>' data-thumbnail='<?php echo $thumbnail; ?>' data-title='<?php echo $title; ?>'>
+                <div class="talks-video-slider__slide-container">
+                  <div class="talks-video-slider__slide-image-container">
+                    <?php echo $thumbnail; ?>
+                    <div class="talks-video-slider__slide-play-btn">
+                      <?php get_template_part('icons/play', null, array('class' => 'talks-video-slider__slide-icon')); ?>
+                    </div>
+                  </div>
+                  <div class="talks-video-slider__slide-title"><?php echo $title; ?></div>
+                </div>
+              </div>
+              <!-- <?php print_r($slide['video_src']); ?> -->
+
+            <?php endforeach; ?>
+
+          </div>
+
+          <div class="talks-video-slider__prev" aria-label='Previous Slide'> 
+            <?php get_template_part('icons/arrow', null, array( 'class' => 'talks-video-slider__icon talks-video-slider__icon--prev' )); ?>
+          </div>
+          <div class="talks-video-slider__next" aria-label='Next Slide'> 
+            <?php get_template_part('icons/arrow', null, array( 'class' => 'talks-video-slider__icon talks-video-slider__icon--next' )); ?>
+          </div>
+          
+          <div class='talks-video-slider__indicators'>
+            <?php $slides = get_field('talks_video_slider_videos');
+              foreach ($slides as $key=>$dot) : ?>
+                  <div class="talks-video-slider__dot" data-index='<?php echo $key; ?>'></div>
+              <?php endforeach;
+            ?>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="talks-video-slider__video-modal">
+      <div class="talks-video-slider__modal-overlay"></div>
+      <div class="talks-video-slider__modal-container">
+        <div class="talks-video-slider__close-btn">close</div>
+        <iframe class="talks-video-slider__video" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"></iframe>
+      </div>
+    </div>
+  <?php endif; ?>
+  <!-- END Talks Video Slider -->
+
   <!-- Talks What You'll Learn Section -->
   <div class="talks-learn">
     <div class="talks-learn__container">
