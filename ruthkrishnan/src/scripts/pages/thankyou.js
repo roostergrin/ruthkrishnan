5eb48b701e7ea3cc0fa11be8cd07a7a065920316
Bryant Fukushima <bmfukushima94@gmail.com>
2021-07-27 17:29:29 -0700

    updates(header & footer): add third party scripts

--- ruthkrishnan/src/scripts/pages/thankyou.js
+++ ruthkrishnan/src/scripts/pages/thankyou.js
@@ -1,44 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
-  
-  let debounceLastTimeout = null;
-  
-  const setPageHeight = () => {
 
-    const header = document.querySelector('.site-navigation'),
-          subscribe = document.querySelector('.form-subscribe'),
-          footer = document.querySelector('.footer'),
-          thankyou = document.querySelector('.page-thank-you__container');
-
-    if (window.innerHeight - (subscribe.offsetHeight + footer.offsetHeight + (header.offsetHeight / 2)) > 280) {
-      thankyou.style.height = `${window.innerHeight - (subscribe.offsetHeight + footer.offsetHeight + (header.offsetHeight / 2))}px`;
-      thankyou.style.marginTop = `${header.offsetHeight / 2}px`;
-    } else {
-      thankyou.style.marginTop = `${header.offsetHeight / 2}px`;
-    }
+    const header = document.querySelector('.site-navigation');
+    
     header.classList.add('site-navigation--active');
+  
+  // let debounceLastTimeout = null;
+  
+  // const setPageHeight = () => {
 
-  }
+  //         subscribe = document.querySelector('.form-subscribe'),
+  //         footer = document.querySelector('.footer'),
+  //         thankyou = document.querySelector('.page-thank-you__container');
 
-  setPageHeight();
+  //   if (window.innerHeight - (subscribe.offsetHeight + footer.offsetHeight + (header.offsetHeight / 2)) > 280) {
+  //     thankyou.style.height = `${window.innerHeight - (subscribe.offsetHeight + footer.offsetHeight + (header.offsetHeight / 2))}px`;
+  //     thankyou.style.marginTop = `${header.offsetHeight / 2}px`;
+  //   } else {
+  //     thankyou.style.marginTop = `${header.offsetHeight / 2}px`;
+  //   }
 
-  // debounce function
-  const debounce = (func, args, wait, immediate) => {
-    const later = () => {
-      debounceLastTimeout = null
-      if (!immediate) {
-        func(args)
-      }
-    };
+  // }
 
-    const callNow = immediate && !debounceLastTimeout
-    clearTimeout(debounceLastTimeout)
-    debounceLastTimeout = setTimeout(later, wait)
-    if (callNow) {
-      func(args)
-    }
-  }
+  // setPageHeight();
 
-  window.addEventListener('resize', () => {
-    debounce(setPageHeight, null, 300)
-  })
+  // // debounce function
+  // const debounce = (func, args, wait, immediate) => {
+  //   const later = () => {
+  //     debounceLastTimeout = null
+  //     if (!immediate) {
+  //       func(args)
+  //     }
+  //   };
+
+  //   const callNow = immediate && !debounceLastTimeout
+  //   clearTimeout(debounceLastTimeout)
+  //   debounceLastTimeout = setTimeout(later, wait)
+  //   if (callNow) {
+  //     func(args)
+  //   }
+  // }
+
+  // window.addEventListener('resize', () => {
+  //   debounce(setPageHeight, null, 300)
+  // })
 });
