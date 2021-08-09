dc3e6da0f31966864fee4f6d90afca94469c3bfb
Bryant Fukushima <bmfukushima94@gmail.com>
2021-08-09 13:08:18 -0700

    updates(single-propertylistings): get correct testimonails

--- ruthkrishnan/single-propertylistings.php
+++ ruthkrishnan/single-propertylistings.php
@@ -243,7 +243,7 @@ get_header(); ?>
 								<div class="testimonials-section__nav testimonials-section__nav--prev">
 									<?php get_template_part('icons/arrow', null, array( 'class' => 'testimonials-section__nav-icon testimonials-section__nav-icon--prev')); ?>
 								</div>
-								<?php foreach (get_field('testimonials') as $key=>$dot) : ?>
+								<?php foreach (get_field('testimonials', $home_ID) as $key=>$dot) : ?>
 									<div class="testimonials-section__dot" data-index='<?php echo $key; ?>'></div>
 								<?php endforeach; ?>
 								<div class="testimonials-section__nav testimonials-section__nav--next">
