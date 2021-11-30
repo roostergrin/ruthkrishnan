22aba901c6e4057c94e145b90e0f5aa4ba5e9f58
Skot Walker <skotivanwalker@gmail.com>
2021-11-30 10:50:39 -0800

    (header.php):adds null alt to facebook pixel code

--- ruthkrishnan/header.php
+++ ruthkrishnan/header.php
@@ -42,7 +42,7 @@ fbq('track', 'PageView');
 </script>
 
 <noscript>
-<img height="1" width="1"
+<img height="1" width="1" alt=""
 src="https://www.facebook.com/tr?id=1871650279749281&ev=PageView&amp;noscript=1"/>
 </noscript>
 <!-- End Facebook Pixel Code -->
