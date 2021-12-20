907deb4949e1674d167c9332e003002cd9fde2dc
tina <longevity.tina@gmail.com>
2021-12-20 11:11:06 -0800

    requests(single-career): Add job description video ticket#56782

--- ruthkrishnan/js/single-careers.js
+++ ruthkrishnan/js/single-careers.js
@@ -3,9 +3,21 @@ var __webpack_exports__ = {};
 /*!*********************************************!*\
   !*** ./src/scripts/pages/single-careers.js ***!
   \*********************************************/
-document.addEventListener('DOMContentLoaded', function () {// const page = document.querySelector('.single-careers')
+document.addEventListener('DOMContentLoaded', function () {
+  var playButton = document.querySelector('.single-careers-video__play-btn'),
+      videoContainer = document.querySelector('.single-careers-video__video-container'),
+      thumbnail = document.querySelector('.single-careers-video__thumbnail'),
+      video = document.querySelector('.single-careers-video__video'); //  page = document.querySelector('.single-careers')
   //       navigation = document.querySelector('.site-navigation');
   // page.style.marginTop = navigation.offsetHeight + 'px'
+
+  video.addEventListener('loadeddata', function () {});
+  videoContainer.addEventListener('click', function () {
+    video.src = video.dataset.src;
+    playButton.classList.add('single-careers-video__play-btn--hidden');
+    thumbnail.classList.add('single-careers-video__thumbnail--hidden');
+    video.classList.add('single-careers-video__video--active');
+  });
 });
 /******/ })()
 ;
