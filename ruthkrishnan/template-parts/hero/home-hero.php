ac10377c2ececa7362de14f297dd71d68e305704
Skot Walker <skotivanwalker@gmail.com>
2022-01-27 13:51:27 -0800

    (home-hero.php):minor track tag tweak

--- ruthkrishnan/template-parts/hero/home-hero.php
+++ ruthkrishnan/template-parts/hero/home-hero.php
@@ -10,5 +10,9 @@
 ?>
 
 <div class="home-hero__overlay"></div>
-<video class="home-hero__video" src="<?php echo get_field('hero_video'); ?>" autoplay controls loop muted playsinline></video>
+<video class="home-hero__video" autoplay controls loop muted playsinline>
+  <source src="<?php echo get_field('hero_video'); ?>" type="video/mp4">
+  <track kind="captions" src="<?php echo get_field('hero_webvtt'); ?>" srclang="en" default>
+  <track kind="descriptions" src="<?php echo get_field('hero_webvtt_ad'); ?>" srclang="en" default>
+</video>
 <!-- <div class="home-hero__pause-btn"></div> -->
