20ac64d9ec2ea710418ed09bc7a2e7a3f2c04a29
qooqu <david.greenwold@gmail.com>
2022-02-17 11:54:56 -0800

    requests(404) updates 404 content and layout #58414

--- ruthkrishnan/404.php
+++ ruthkrishnan/404.php
@@ -9,8 +9,20 @@ get_header(); ?>
 
 
 <div class='page-404'>
-  <h1 class='page-404__title'>Page Not Found.</h1>
-  <a href="/" class="page-404__btn">return to homepage</a>
+  <div class='page-404__overlay'></div>
+  <div class='page-404__container'>
+    <h1 class='page-404__title'>The page you're looking for has moved!</h1>
+    <h2 class='page-404__subtitle'>Let us help you find what you're looking for.</h2>
+    <div class='page-404__btnContainer'>
+      <a href="/" class="page-404__btn">Home</a>
+      <a href="/blog/market-updates/" class="page-404__btn">Market Updates</a>
+      <a href="/buy/" class="page-404__btn">Buying a Home</a>
+      <a href="/sell/" class="page-404__btn">Selling a Home</a>
+      <a href="/new-developments/" class="page-404__btn">New Developments</a>
+      <a href="/neighborhoods/" class="page-404__btn">Neighborhoods</a>
+      <a href="/contact-us/" class="page-404__btn">Contact Us</a>
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
