f2dfac27c6e7beb60324a8b31d81c8cf90f4ed4d
Gordon Lewis <glewis@berkeley.edu>
2022-08-29 16:00:28 -0700

    Updates tooltips

--- ruthkrishnan/js/neighborhoods.js
+++ ruthkrishnan/js/neighborhoods.js
@@ -120,19 +120,21 @@ var sliderNeighborhoods = function sliderNeighborhoods() {
   }
 
   allSlides.forEach(function (slide) {
-    if (slide.HJISingleMonthly.result.count != 0) {
-      if (calculateHotScore(slide.HJISingleMonthly.result.measurements) != 0) {
-        console.log('single', slide.neighborhood, calculateHotScore(slide.HJISingleMonthly.result.measurements), slide.HJISingleMonthly.result.count);
-        if (calculateHotScore(slide.HJISingleMonthly.result.measurements) < minHotScoreHouse) minHotScoreHouse = calculateHotScore(slide.HJISingleMonthly.result.measurements);
-        if (calculateHotScore(slide.HJISingleMonthly.result.measurements) > maxHotScoreHouse) maxHotScoreHouse = calculateHotScore(slide.HJISingleMonthly.result.measurements);
+    if (slide.HJISingleMonthly.result) {
+      if (slide.HJISingleMonthly.result.count != 0) {
+        if (calculateHotScore(slide.HJISingleMonthly.result.measurements) != 0) {
+          if (calculateHotScore(slide.HJISingleMonthly.result.measurements) < minHotScoreHouse) minHotScoreHouse = calculateHotScore(slide.HJISingleMonthly.result.measurements);
+          if (calculateHotScore(slide.HJISingleMonthly.result.measurements) > maxHotScoreHouse) maxHotScoreHouse = calculateHotScore(slide.HJISingleMonthly.result.measurements);
+        }
       }
     }
 
-    if (slide.HJICondoMonthly.result.count != 0) {
-      if (calculateHotScore(slide.HJICondoMonthly.result.measurements) != 0) {
-        console.log('condo', slide.neighborhood, calculateHotScore(slide.HJICondoMonthly.result.measurements), slide.HJICondoMonthly.result.count);
-        if (calculateHotScore(slide.HJICondoMonthly.result.measurements) < minHotScoreCondo) minHotScoreCondo = calculateHotScore(slide.HJICondoMonthly.result.measurements);
-        if (calculateHotScore(slide.HJICondoMonthly.result.measurements) > maxHotScoreCondo) maxHotScoreCondo = calculateHotScore(slide.HJICondoMonthly.result.measurements);
+    if (slide.HJISingleMonthly.result) {
+      if (slide.HJICondoMonthly.result.count != 0) {
+        if (calculateHotScore(slide.HJICondoMonthly.result.measurements) != 0) {
+          if (calculateHotScore(slide.HJICondoMonthly.result.measurements) < minHotScoreCondo) minHotScoreCondo = calculateHotScore(slide.HJICondoMonthly.result.measurements);
+          if (calculateHotScore(slide.HJICondoMonthly.result.measurements) > maxHotScoreCondo) maxHotScoreCondo = calculateHotScore(slide.HJICondoMonthly.result.measurements);
+        }
       }
     }
   });
@@ -312,6 +314,7 @@ var sliderNeighborhoods = function sliderNeighborhoods() {
       }
 
       value = el.dataset.filter;
+      closeToolTip();
       colorIconArr(value);
     });
     el.addEventListener("keyup", function () {
@@ -321,6 +324,7 @@ var sliderNeighborhoods = function sliderNeighborhoods() {
       }
 
       value = el.dataset.filter;
+      closeToolTip();
       colorIconArr(value);
     });
   });
