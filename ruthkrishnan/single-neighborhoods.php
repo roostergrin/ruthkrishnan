e8afb96c603d0c810036b295c1b7a286918a696d
Dave <qooqu@users.noreply.github.com>
2022-09-08 17:10:59 -0700

    requests(neighborhoods) updates json parsing #64008

--- ruthkrishnan/single-neighborhoods.php
+++ ruthkrishnan/single-neighborhoods.php
@@ -26,7 +26,10 @@ get_header(); ?>
 					<div class="single-neighborhoods-content__column">
 						<h2 class="single-neighborhoods-content__title"><?php echo the_title(); ?></h2>
 						<div class="single-neighborhoods-content__text"><?php echo get_field('description'); ?></div>
-						<div class="single-neighborhoods-content__data" data-neighborhoodHJISingle='<?php echo get_field('single_data'); ?>' data-neighborhoodHJICondo='<?php echo get_field('condo_data'); ?>'></div>
+						<div class="single-neighborhoods-content__data"
+							 data-neighborhoodHJISingle='<?php echo get_field("single_data"); ?>'
+							 data-neighborhoodHJICondo='<?php echo get_field("condo_data"); ?>'
+						></div>
 						<div class="single-neighborhoods-content__icons">
 							<?php if (have_rows('single_neighborhoods_icons')) :
 								while (have_rows('single_neighborhoods_icons')) : the_row();
