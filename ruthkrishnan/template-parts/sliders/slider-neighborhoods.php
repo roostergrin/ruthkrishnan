990bbf1eba89429fda76696d731d03a37671b1c6
Gordon Lewis <glewis@berkeley.edu>
2022-09-13 10:08:58 -0700

    Updates map

--- ruthkrishnan/template-parts/sliders/slider-neighborhoods.php
+++ ruthkrishnan/template-parts/sliders/slider-neighborhoods.php
@@ -23,119 +23,155 @@
   </div>
   <div id="legend" class="slider-neighborhoods__legend-container">
   </div>
-  <?php get_template_part('template-parts/maps/neighborhoods-map'); ?>
-</div>
-<div class="slider-neighborhoods__slider">
-  <?php
-  $args = array(
-    'post_type' => 'neighborhoods',
-    'posts_per_page' => -1,
-    'post_status' => 'publish',
-    'orderby' => 'title',
-    'order' => 'ASC',
-  );
-
-  $query = new WP_Query($args); ?>
-
-  <!-- Loops through posts and displays the neighborhood images -->
-  <div id="slider-container" class="slider-neighborhoods__slider-container">
-    <div class="slider-neighborhoods__track">
-      <?php if ($query->have_posts()) :
-        while ($query->have_posts()) : $query->the_post();
-
-          $category = get_the_terms(get_post()->ID, 'neighborhood-category')[0]->slug;
-
-          if (have_rows('background_image')) :
-            while (have_rows('background_image')) : the_row();
-              $image = get_sub_field('image'); ?>
-
-              <div class="slider-neighborhoods__slide"
-				   style="<?php if ($category === 'coming-soon') : echo 'display: none'; endif; ?>"
-				   data-name='<?php echo the_title(); ?>'
-				   data-mapinfo='<?php echo json_encode(get_field("map_info_window")); ?>'
-				   data-hjisinglemonthly='<?php echo get_field("single_last_month"); ?>'
-				   data-hjicondomonthly='<?php echo get_field("condo2br2b_data"); ?>'
-				   data-walkscore='<?php echo get_field("walk_score"); ?>'
-				   data-transitscore='<?php echo get_field("transit_score"); ?>'
-				   data-weather='<?php echo get_field("weather"); ?>'
-				   data-neighborhood='<?php echo get_post()->post_name; ?>'
-				   data-category='<?php echo $category ?>'
-				>
-                <div class="slider-neighborhoods__image-container">
-                  <?php echo wp_get_attachment_image($image, 'medium_large', false, ['class' => 'slider-neighborhoods__image']); ?>
-                </div>
-              </div>
-
-
-          <?php endwhile;
-          endif; ?>
-
-      <?php endwhile;
-      endif; ?>
+  <div class="map-neighborhoods">
+    <div class="map-neighborhoods__tooltip">
+      <div id="tooltip-close" class="map-neighborhoods__tooltip-close"></div>
+      <div id="tooltip-content" class="map-neighborhoods__tooltip-content"></div>
     </div>
-  </div>
-  <div class="slider-neighborhoods__carousel-controls-container">
-    <div id="previous" class="slider-neighborhoods__previous-wrapper">
-      <?php get_template_part('icons/arrow', null, array( 'class' => 'slider-neighborhoods__previous-arrow')); ?>
-    </div>
-    <div class="slider-neighborhoods__pagination-container">
-      <div class="slider-neighborhoods__pagination-line"></div>
-      <div id="pagination-indicator" class="slider-neighborhoods__pagination-indicator"></div>
-    </div>
-    <div id="next" class="slider-neighborhoods__next-wrapper">
-      <?php get_template_part('icons/arrow', null, array( 'class' => 'slider-neighborhoods__next-arrow')); ?>
-    </div>
-    </div>
-  <!-- Loops through the posts and displays neighborhood descriptions -->
-  <div class="slider-neighborhoods__content-container">
-    <!-- <div class="slider-neighborhoods__content-fade"></div> -->
-    <div class="slider-neighborhoods__content-column">
-      <?php
-      $active_args = array(
-        'post_type' => 'neighborhoods',
-        'posts_per_page' => -1,
-        'post_status' => 'publish',
-        'orderby' => 'title',
-        'order' => 'ASC',
-        'tax_query' => array(
-          array(
-            'taxonomy' => 'neighborhood-category',
-            'field' => 'slug',
-            'terms' => 'active'
-          )
-        )
-      );
+    <div class="map-neighborhoods__container">
+      <div class="map-neighborhoods__column">
+        <?php get_template_part('icons/map', null, array('class' => 'map-neighborhoods__icon')); ?>
+      </div>
+      <div class="map-neighborhoods__column slider-neighborhoods__slider">
+        <?php
+        $args = array(
+          'post_type' => 'neighborhoods',
+          'posts_per_page' => -1,
+          'post_status' => 'publish',
+          'orderby' => 'title',
+          'order' => 'ASC',
+        );
 
-      $active_query = new WP_Query($active_args); ?>
-      <?php if ($active_query->have_posts()) :
-        while ($active_query->have_posts()) : $active_query->the_post(); ?>
-          <div class="slider-neighborhoods__content-wrapper" data-index='<?php echo $active_query->current_post; ?>'>
-            <div class="slider-neighborhoods__content">
-              <h3 class="slider-neighborhoods__content-title"><?php echo the_title(); ?></h3>
-              <div class="slider-neighborhoods__content-description">
-                <?php echo get_field('description'); ?>
-              </div>
-              <?php if (have_rows('single_neighborhoods_icons')) : ?>
-                <div class="slider-neighborhoods__content-icons">
-                  <?php while (have_rows('single_neighborhoods_icons')) : the_row();
+        $query = new WP_Query($args); ?>
 
-                    $icon = get_sub_field('icon');
-                    $text = get_sub_field('text'); ?>
+        <!-- Loops through posts and displays the neighborhood images -->
+        <div id="slider-container" class="slider-neighborhoods__slider-container">
+          <div class="slider-neighborhoods__track">
+            <?php if ($query->have_posts()) :
+              while ($query->have_posts()) : $query->the_post();
 
-                    <div class="slider-neighborhoods__content-icon-container">
-                      <?php get_template_part('icons/' . $icon, null, array('class' => 'slider-neighborhoods__content-icon')); ?>
-                      <div class="slider-neighborhoods__content-icon-text"><?php echo $text; ?></div>
+                $category = get_the_terms(get_post()->ID, 'neighborhood-category')[0]->slug;
+
+                if (have_rows('background_image')) :
+                  while (have_rows('background_image')) : the_row();
+                    $image = get_sub_field('image'); ?>
+
+                    <div class="slider-neighborhoods__slide" style="<?php if ($category === 'coming-soon') : echo 'display: none';
+                                                                    endif; ?>" data-name='<?php echo the_title(); ?>' data-mapinfo='<?php echo json_encode(get_field("map_info_window")); ?>' data-hjisinglemonthly='<?php echo get_field("single_last_month"); ?>' data-hjicondomonthly='<?php echo get_field("condo2br2b_data"); ?>' data-walkscore='<?php echo get_field("walk_score"); ?>' data-transitscore='<?php echo get_field("transit_score"); ?>' data-weather='<?php echo get_field("weather"); ?>' data-neighborhood='<?php echo get_post()->post_name; ?>' data-category='<?php echo $category ?>'>
+                      <!-- <div class="slider-neighborhoods__image-container">
+                        <?php echo wp_get_attachment_image($image, 'medium_large', false, ['class' => 'slider-neighborhoods__image']); ?>
+                      </div> -->
                     </div>
 
-                  <?php endwhile; ?>
-                </div>
-              <?php endif; ?>
-              <a class="slider-neighborhoods__content-link" href="/neighborhoods/<?php echo get_post()->post_name; ?>" aria-label="learn more about <?php echo get_post()->post_name; ?> neighborhood">Learn More</a>
-            </div>
+
+                <?php endwhile;
+                endif; ?>
+
+            <?php endwhile;
+            endif; ?>
           </div>
-        <?php endwhile; ?>
-      <?php endif; ?>
+        </div>
+        <!-- Loops through the posts and displays neighborhood descriptions -->
+        <div class="slider-neighborhoods__content-container">
+          <!-- <div class="slider-neighborhoods__content-fade"></div> -->
+          <div class="slider-neighborhoods__content-column">
+            <?php
+            $active_args = array(
+              'post_type' => 'neighborhoods',
+              'posts_per_page' => -1,
+              'post_status' => 'publish',
+              'orderby' => 'title',
+              'order' => 'ASC',
+              'tax_query' => array(
+                array(
+                  'taxonomy' => 'neighborhood-category',
+                  'field' => 'slug',
+                  'terms' => 'active'
+                )
+              )
+            );
+
+            $active_query = new WP_Query($active_args); ?>
+            <?php if ($active_query->have_posts()) :
+              while ($active_query->have_posts()) : $active_query->the_post(); ?>
+                <div class="slider-neighborhoods__content-wrapper" data-index='<?php echo $active_query->current_post; ?>'>
+                  <div class="slider-neighborhoods__content">
+                    <h3 class="slider-neighborhoods__content-title"><?php echo the_title(); ?></h3>
+                    <div class="slider-neighborhoods__content-description">
+                      <div class="slider-neighborhoods__score-row">
+                        <div class="slider-neighborhoods__score">
+                          <p id="transit-score-display" class="slider-neighborhoods__score-content">90</p>
+                          <p class="slider-neighborhoods__score-label">transit score</p>
+                        </div>
+                        <div class="slider-neighborhoods__score">
+                          <p id="walk-score-display" class="slider-neighborhoods__score-content">92</p>
+                          <p class="slider-neighborhoods__score-label">walk score</p>
+                        </div>
+                        <div class="slider-neighborhoods__score">
+                          <p id="weather-score-display" class="slider-neighborhoods__score-content">42°-78°</p>
+                          <p class="slider-neighborhoods__score-label">weather score</p>
+                        </div>
+                      </div>
+                      <div class="slider-neighborhoods__price-row">
+                        <p class="slider-neighborhoods__price-heading">House</p>
+                        <div class="slider-neighborhoods__price">
+                          <p id="weather-score-display" class="slider-neighborhoods__price-content">$5,555,555</p>
+                          <p class="slider-neighborhoods__price-label">median sale price</p>
+                        </div>
+                        <div class="slider-neighborhoods__price-sq-ft">
+                          <p id="weather-score-display" class="slider-neighborhoods__price-content">$5,555.23</p>
+                          <p class="slider-neighborhoods__price-label">price to sq footage</p>
+                        </div>
+                      </div>
+
+                      <div class="slider-neighborhoods__price-row">
+                        <p class="slider-neighborhoods__price-heading">2BR/2BA Condo</p>
+                        <div class="slider-neighborhoods__price">
+                          <p id="weather-score-display" class="slider-neighborhoods__price-content">$c5,555,555</p>
+                          <p class="slider-neighborhoods__price-label">median sale price</p>
+                        </div>
+                        <div class="slider-neighborhoods__price-sq-ft">
+                          <p id="weather-score-display" class="slider-neighborhoods__price-content">$c5,555.23</p>
+                          <p class="slider-neighborhoods__price-label">price to sq footage</p>
+                        </div>
+                      </div>
+                      <?php echo get_field('description'); ?>
+                    </div>
+                    <?php if (have_rows('single_neighborhoods_icons')) : ?>
+                      <div class="slider-neighborhoods__content-icons">
+                        <?php while (have_rows('single_neighborhoods_icons')) : the_row();
+
+                          $icon = get_sub_field('icon');
+                          $text = get_sub_field('text'); ?>
+
+                          <div class="slider-neighborhoods__content-icon-container">
+                            <?php get_template_part('icons/' . $icon, null, array('class' => 'slider-neighborhoods__content-icon')); ?>
+                            <div class="slider-neighborhoods__content-icon-text"><?php echo $text; ?></div>
+                          </div>
+
+                        <?php endwhile; ?>
+                      </div>
+                    <?php endif; ?>
+                    <a class="slider-neighborhoods__content-link" href="/neighborhoods/<?php echo get_post()->post_name; ?>" aria-label="learn more about <?php echo get_post()->post_name; ?> neighborhood">Learn More</a>
+                  </div>
+                </div>
+              <?php endwhile; ?>
+            <?php endif; ?>
+          </div>
+        </div>
+        <div class="slider-neighborhoods__carousel-controls-container">
+          <div id="previous" class="slider-neighborhoods__previous-wrapper">
+            <?php get_template_part('icons/arrow', null, array('class' => 'slider-neighborhoods__previous-arrow')); ?>
+          </div>
+          <div class="slider-neighborhoods__pagination-container">
+            <div class="slider-neighborhoods__pagination-line"></div>
+            <div id="pagination-indicator" class="slider-neighborhoods__pagination-indicator"></div>
+          </div>
+          <div id="next" class="slider-neighborhoods__next-wrapper">
+            <?php get_template_part('icons/arrow', null, array('class' => 'slider-neighborhoods__next-arrow')); ?>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</div>
 </div>
