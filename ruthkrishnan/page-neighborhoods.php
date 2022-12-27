95ce505e059808ae15aeee3792815f8b3737f013
phamiter253 <phamiter253@gmail.com>
2022-12-27 11:07:01 -0800

    request(page-neighborhoods.php) Adding resources links to neighborhood page - fixed #66725

--- ruthkrishnan/page-neighborhoods.php
+++ ruthkrishnan/page-neighborhoods.php
@@ -21,7 +21,98 @@ get_header(); ?>
   <!-- END Slider Neighborhoods Template Part -->
 
   <!-- Resources Links Template Part -->
-  <?php get_template_part('template-parts/resources-links/resources-links'); ?>
+  <div class="resources-links">
+				<div class="resources-links__container">
+					<img width="1280" height="572" src="https://d1nkwan2sp5tun.cloudfront.net/wp-content/uploads/2021/06/10-column-bg.jpg" class="resources-links__background entered lazyloaded" alt="Neighborhoods" data-lazy-srcset="https://d1nkwan2sp5tun.cloudfront.net/wp-content/uploads/2021/06/10-column-bg.jpg 1280w,https://d1nkwan2sp5tun.cloudfront.net/wp-content/uploads/2021/06/10-column-bg-768x343.jpg 768w" data-lazy-sizes="(max-width: 1280px) 100vw, 1280px" data-lazy-src="https://d1nkwan2sp5tun.cloudfront.net/wp-content/uploads/2021/06/10-column-bg.jpg" data-ll-status="loaded" sizes="(max-width: 1280px) 100vw, 1280px" srcset="https://d1nkwan2sp5tun.cloudfront.net/wp-content/uploads/2021/06/10-column-bg.jpg 1280w,https://d1nkwan2sp5tun.cloudfront.net/wp-content/uploads/2021/06/10-column-bg-768x343.jpg 768w"><noscript><img width="1280" height="572" src="https://d1nkwan2sp5tun.cloudfront.net/wp-content/uploads/2021/06/10-column-bg.jpg" class="resources-links__background" alt="Neighborhoods" srcset="https://d1nkwan2sp5tun.cloudfront.net/wp-content/uploads/2021/06/10-column-bg.jpg 1280w,https://d1nkwan2sp5tun.cloudfront.net/wp-content/uploads/2021/06/10-column-bg-768x343.jpg 768w" sizes="(max-width: 1280px) 100vw, 1280px" /></noscript>
+					<h2 class="resources-links__title">Buyers Resources</h2>
+					<div class="resources-links__links">
+
+					<a href="/blog/buying-a-home/" class="resources-links__link-container" style="height: 78px;">
+							<div class="resources-links__link-text-container">
+								<div class="resources-links__link-background"></div>
+								<p class="resources-links__link-text">Homebuyer resources</p>
+							</div>
+							<div class="resources-links__icon-container">
+
+								<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="resources-links__icon">
+									<path d="M14.78,7.67H0v.66H14.78L10,13h.9L16,8,10.85,3H10Z" style="fill: #232323"></path>
+								</svg>
+							</div>
+						</a>
+
+
+						<a href="/hire-an-agent-before-you-go-to-an-open-house/" class="resources-links__link-container" style="height: 78px;">
+							<div class="resources-links__link-text-container">
+								<div class="resources-links__link-background"></div>
+								<p class="resources-links__link-text">Why hire an agent before going to an open house</p>
+							</div>
+							<div class="resources-links__icon-container">
+
+								<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="resources-links__icon">
+									<path d="M14.78,7.67H0v.66H14.78L10,13h.9L16,8,10.85,3H10Z" style="fill: #232323"></path>
+								</svg>
+							</div>
+						</a>
+
+
+						<a href="/advantages-of-a-dedicated-buyer-agent/" class="resources-links__link-container" style="height: 78px;">
+							<div class="resources-links__link-text-container">
+								<div class="resources-links__link-background"></div>
+								<p class="resources-links__link-text">Advantages of a dedicated buyer's agent</p>
+							</div>
+							<div class="resources-links__icon-container">
+
+								<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="resources-links__icon">
+									<path d="M14.78,7.67H0v.66H14.78L10,13h.9L16,8,10.85,3H10Z" style="fill: #232323"></path>
+								</svg>
+							</div>
+						</a>
+
+
+						<a href="/the-home-search/" class="resources-links__link-container" style="height: 78px;">
+							<div class="resources-links__link-text-container">
+								<div class="resources-links__link-background"></div>
+								<p class="resources-links__link-text">Your home search</p>
+							</div>
+							<div class="resources-links__icon-container">
+
+								<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="resources-links__icon">
+									<path d="M14.78,7.67H0v.66H14.78L10,13h.9L16,8,10.85,3H10Z" style="fill: #232323"></path>
+								</svg>
+							</div>
+						</a>
+
+
+						<a href="/what-is-liquefaction/" class="resources-links__link-container" style="height: 78px;">
+							<div class="resources-links__link-text-container">
+								<div class="resources-links__link-background"></div>
+								<p class="resources-links__link-text">What is liquefaction</p>
+							</div>
+							<div class="resources-links__icon-container">
+
+								<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="resources-links__icon">
+									<path d="M14.78,7.67H0v.66H14.78L10,13h.9L16,8,10.85,3H10Z" style="fill: #232323"></path>
+								</svg>
+							</div>
+						</a>
+
+
+						<a href="/san-francisco-microclimates-and-impact-on-home-prices/" class="resources-links__link-container" style="height: 78px;">
+							<div class="resources-links__link-text-container">
+								<div class="resources-links__link-background"></div>
+								<p class="resources-links__link-text">Microclimates and impact on home prices</p>
+							</div>
+							<div class="resources-links__icon-container">
+
+								<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="resources-links__icon">
+									<path d="M14.78,7.67H0v.66H14.78L10,13h.9L16,8,10.85,3H10Z" style="fill: #232323"></path>
+								</svg>
+							</div>
+						</a>
+
+					</div>
+				</div>
+			</div>
   <!-- END Resources Links Template Part -->
 
   <!-- Testimonials Template Part -->
