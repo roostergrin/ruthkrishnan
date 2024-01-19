a0353b8e93645288cf732f5f7d5136ef738134f4
Sebastian Park <me@sebastian-park.com>
2024-01-18 16:29:01 -0800

    requests(page-survey) conditionally render component 77653

--- ruthkrishnan/template-parts/forms/form-survey-open-house.php
+++ ruthkrishnan/template-parts/forms/form-survey-open-house.php
@@ -27,6 +27,7 @@
             <div class="form-survey__form-container">
               <form action="" class="form-survey__form" id="survey-form" data-page='<?php echo the_title(); ?>'>
                 <div class="form-survey__form-group">
+                  <!-- TEST TEST -->
                   <label for="fullname" class="form-survey__label">First and Last Name</label>
                   <input type="text" name="fullname" id="fullname" placeholder="First and Last Name*" class="form-survey__input">
                   <small class="form-survey__validation-message" id="fullname-validation">Please enter valid full name.</small>
