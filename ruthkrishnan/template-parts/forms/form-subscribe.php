b08523be155af58e0887da1f3a9929903228b62e
jason murphy <jmurphy.1196@gmail.com>
2024-08-07 16:41:26 -0700

    requests fixes newletter link #83062

--- ruthkrishnan/template-parts/forms/form-subscribe.php
+++ ruthkrishnan/template-parts/forms/form-subscribe.php
@@ -15,7 +15,7 @@
       <h2 class="form-subscribe__title"><?php echo get_field('subscribe_title', 'options'); ?></h2>
       <div class="form-subscribe__text"><?php echo get_field('subscribe_text', 'options'); ?></div>
     </div>
-    <a href="https://paragon-re.us5.list-manage.com/subscribe/post?u=c8474b2af5037cd0c2709da5c&id=47357a5c28" class="form-subscribe__form-column" target="_blank" aria-label="subscribe now on subscription page, opens in new window.">
+    <a href="https://ruthkrishnan.us5.list-manage.com/subscribe?u=c8474b2af5037cd0c2709da5c&id=47357a5c28" class="form-subscribe__form-column" target="_blank" aria-label="subscribe now on subscription page, opens in new window.">
       <button class="form-subscribe__button">subscribe now</button>
     </a>
   </div>
